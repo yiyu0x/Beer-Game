ea50ca64a915671dd7fb2cc3a5711bb03e82005d
yiyu0x <tako0903@icloud.com>
2018-12-21 22:59:48 +0800

    login system done

--- server/app.js
+++ server/app.js
@@ -6,10 +6,24 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//CORS fixed
+app.all('*', function (req, res, next) {
+    // console.log('debug: ', req.method)
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-type');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'PUT,POST,GET,DELETE,OPTIONS,PATCH'
+    );
+    res.header('Access-Control-Max-Age', 1728000);
+    next();
+});
+
 app.post('/login', (req, res) => {
     var UserName = req.body.username;
     var UserPasswd = req.body.password;
     console.log(UserName, UserPasswd)
+    console.log(req.body)
     var rowData = { 'username': UserName, 'passwd': UserPasswd};
     Mongo.find(rowData, function(err, obj) {
         if (err) {
--- src/components/ButtonBox.vue
+++ src/components/ButtonBox.vue
@@ -1,74 +1,98 @@
 <template>
-  <v-container>
-    <v-layout row justify-center>
-      <v-dialog v-model="dialog_login" persistent max-width="600px">
-        <v-btn slot="activator" dark>登入</v-btn>
-        <v-card>
-          <v-card-title>
-            <span class="headline">登入</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12>
-                  <v-text-field label="User name" required></v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                  <v-text-field label="Password" type="password" required></v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="dialog_login = false">關閉</v-btn>
-            <v-btn color="blue darken-1" flat @click="login()">登入</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-layout>
-    <v-layout row justify-center>
-      <v-dialog v-model="dialog_new" persistent max-width="600px">
-        <v-btn slot="activator" dark>註冊帳號</v-btn>
-        <v-card>
-          <v-card-title>
-            <span class="headline">註冊</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12>
-                  <v-text-field label="User name" required></v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                  <v-text-field label="Password" type="password" required></v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="dialog_new = false">關閉</v-btn>
-            <v-btn color="blue darken-1" flat @click="submit()">送出</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-layout>
-  </v-container>
+    <v-container>
+        <v-layout row justify-center>
+            <v-dialog v-model="dialog_login" persistent max-width="600px">
+                <v-btn slot="activator" dark>登入</v-btn>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">登入</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex xs12>
+                                    <v-text-field v-model="loginUserName" label="User name" required></v-text-field>
+                                </v-flex>
+                                <v-flex xs12>
+                                    <v-text-field v-model="loginUserPasswd" label="Password" type="password" required></v-text-field>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click="dialog_login = false">關閉</v-btn>
+                        <v-btn color="blue darken-1" flat @click="login()">登入</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-layout>
+        <v-layout row justify-center>
+            <v-dialog v-model="dialog_new" persistent max-width="600px">
+                <v-btn slot="activator" dark>註冊帳號</v-btn>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">註冊</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex xs12>
+                                    <v-text-field label="User name" required></v-text-field>
+                                </v-flex>
+                                <v-flex xs12>
+                                    <v-text-field label="Password" type="password" required></v-text-field>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click="dialog_new = false">關閉</v-btn>
+                        <v-btn color="blue darken-1" flat @click="submit()">送出</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 export default {
-  data: () => ({
-    dialog_login: false,
-    dialog_new: false
-  }),
-  methods: {
-    login() {
-      this.dialog_login = false;
-    },
-    submit() {
-      this.dialog_new = false;
+    data: () => ({
+        dialog_login: false,
+        dialog_new: false,
+        loginUserName: '',
+        loginUserPasswd: ''
+    }),
+    methods: {
+        login() {
+            // this.dialog_login = false;
+            const data = {
+                username: this.loginUserName,
+                password: this.loginUserPasswd
+            }
+            // console.log(this.loginUserName, this.loginUserPasswd)
+            fetch('http://localhost:3000/login', { 
+              method: 'post', 
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify(data)
+            }).then(function(response) {
+              return response.json()
+            }).then((jsonData) => {
+              if (jsonData.status == 1) {
+                // 登入成功
+                console.log('登入成功')
+              } else {
+                // 登入失敗
+                console.log('登入失敗')
+              }
+            }).catch(function(err) {
+              console.log(err);
+            })
+        },
+        submit() {
+            this.dialog_new = false;
+        }
     }
-  }
 };
 </script>
